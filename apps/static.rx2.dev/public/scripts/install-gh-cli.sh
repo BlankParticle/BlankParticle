fb52f54cd5be087bbb11798bae7bc9646257ed99
BlankParticle <blankparticle@gmail.com>
2026-07-11 20:30:32 +0530

    feat: merge in static file host

---
+++ apps/static.rx2.dev/public/scripts/install-gh-cli.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+apt update && apt install -y --no-install-recommends curl
+mkdir -p /etc/apt/keyrings
+curl -fsSL https://cli.github.com/packages/githubcli-archive-keyring.gpg | tee /etc/apt/keyrings/githubcli-archive-keyring.gpg >/dev/null
+chmod a+r /etc/apt/keyrings/githubcli-archive-keyring.gpg
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/githubcli-archive-keyring.gpg] https://cli.github.com/packages stable main" >/etc/apt/sources.list.d/github-cli.list
+apt update && apt install -y --no-install-recommends gh
